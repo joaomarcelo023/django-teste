be72f221db76d036847983fe5bf64ca47d75c08a
django-teste23 <joaomarceloguima23@gmail.com>
2024-03-13 17:00:03 -0300

    commit

--- static/banner.js
+++ static/banner.js
@@ -49,74 +49,10 @@ control.forEach(control => {
     control.addEventListener("click", () => handleClick(isLeft));
 });
 
-
-const imageContainers  = document.getElementsByClassName('container-custom');
-const image = document.getElementById('image');
-const arrowLeftButton = document.getElementsByClassName('arrow-left');
-const arrowRightButton = document.getElementsByClassName('arrow-right');
-console.log(arrowRightButton);
-
-let isDragging = false;
-let startX = 0;
-let currentX = 0;
-let translateX = 0;
-
-if (imageContainers.length > 0) {
-    // Iterar sobre a coleção de elementos
-    for (let i = 0; i < imageContainers.length; i++) {
-        const imageContainer = imageContainers[i];
-
-        // Adicionar o ouvinte de evento a cada elemento individualmente
-        imageContainer.addEventListener('mousedown', startDrag);
-        imageContainer.addEventListener('touchstart', startDrag);
-    }
-} else {
-    console.error('Nenhum elemento com a classe "container-custom" encontrado.');
-}
-
-function startDrag(event) {
-    event.preventDefault();
-
-    if (event.touches) {
-        startX = event.touches[0].clientX;
-    } else {
-        startX = event.clientX;
-    }
-
-    isDragging = true;
-
-    document.addEventListener('mousemove', drag);
-    document.addEventListener('touchmove', drag);
-    document.addEventListener('mouseup', stopDrag);
-    document.addEventListener('touchend', stopDrag);
-}
-
-function drag(event) {
-    if (!isDragging) return;
-
-    event.preventDefault();
-
-    if (event.touches) {
-        currentX = event.touches[0].clientX;
-    } else {
-        currentX = event.clientX;
-    }
-
-}
-
-function stopDrag() {
-        translateX = currentX - startX;
-        console.log(translateX);
-
-        if (translateX > 0) {
-        handleClick(0)
-    } else {
-        handleClick(1)
-    }
-
-    isDragging = false;
-    document.removeEventListener('mousemove', drag);
-    document.removeEventListener('touchmove', drag);
-    document.removeEventListener('mouseup', stopDrag);
-    document.removeEventListener('touchend', stopDrag);
-}
+// Adicionar evento de clique para dispositivos móveis
+control.forEach(element => {
+    element.addEventListener('click', function() {
+        // Adicionar classe temporária para simular o estado hover
+        console.log("clicou")
+    });
+});
