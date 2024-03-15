276507e9f37f849fe58c593c7baaf09f97673ebd
django-teste23 <joaomarceloguima23@gmail.com>
2024-03-15 17:19:36 -0300

    commit

--- static/pop_up.js
+++ static/pop_up.js
@@ -2,12 +2,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.getElementById('menuButton');
     const popupMenu = document.getElementById('popupMenu');
     const closeButton = document.getElementById('closeButton');
+    let interruptor = 0;
 
     menuButton.addEventListener('click', function () {
-        popupMenu.style.display = 'block';
+
+        if (interruptor ==0){
+
+        popupMenu.style.display = 'grid';
         setTimeout(function () {
         popupMenu.style.opacity = 1;
         }, 100);
+        interruptor = 1;
+
+        }else{
+
+        popupMenu.style.opacity = 0;
+        setTimeout(function () {
+            popupMenu.style.display = 'none';
+            menuButton.style.display = 'block'
+        }, 500);
+        interruptor = 0;
+
+        }
     });
 
     closeButton.addEventListener('click', function () {
@@ -16,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
             popupMenu.style.display = 'none';
             menuButton.style.display = 'block'
         }, 500);
-
+        interruptor = 0;
     });
 
 
