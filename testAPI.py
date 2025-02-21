bece54f7a710e22f025b8ae68bf1162b9bfcee17
JGAlvarez <joao.g.m.alvarez@gmail.com>
2025-02-20 22:32:14 -0300

    API de novo

--- testAPI.py
+++ testAPI.py
@@ -19,7 +19,12 @@ def post(_site, _message):
         print("Erro ao cadastrar produto:", response.status_code)
 
 def get(_site):
-    response = requests.get(_site)
+    headers = {
+        "Authorization": "Api-Key " + settings.TESTKEY_API_CASAHG,
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(_site, headers=headers)
 
     if response.status_code == 200:
         produtos = response.json()
