493c5f00cb4a738f98f51fc37633da8f4ab1e1fe
JGAlvarez <joao.g.m.alvarez@gmail.com>
2025-04-03 16:49:53 -0300

    Layout do produto pequeno

--- static/js/home.js
+++ static/js/home.js
@@ -13,3 +13,18 @@ window.onload = function () {
         }
     }
 };
+
+const mobile = document.querySelector(".pequeno");
+const divPreco = document.querySelectorAll(".divPreco");
+
+if (window.getComputedStyle(mobile).display === "none") {
+    divPreco.forEach(div => {
+        div.classList.remove("col-8");
+        div.classList.add("col-9");
+    });
+} else {
+    divPreco.forEach(div => {
+        div.classList.add("col-8");
+        div.classList.remove("col-9");
+    });
+}
