47905da1faa8d2d36b96cfc63e3b5c2183830875
JGAlvarez <joao.g.m.alvarez@gmail.com>
2025-04-14 17:30:07 -0300

    produto pequeno layout

--- static/js/produtodetalhe.js
+++ static/js/produtodetalhe.js
@@ -107,3 +107,18 @@ variacaoFacesJanelaClose.addEventListener("click", () => {
         variacaoFacesWindow.style.display = "none";
     }
 });
+
+const mobile = document.querySelector(".pequeno");
+const divPreco = document.querySelectorAll(".divPreco");
+
+if (window.getComputedStyle(mobile).display === "none") {
+    divPreco.forEach(div => {
+        div.classList.remove("col-8");
+        div.classList.add("col-9");
+    });
+} else {
+    divPreco.forEach(div => {
+        div.classList.add("col-8");
+        div.classList.remove("col-9");
+    });
+}
