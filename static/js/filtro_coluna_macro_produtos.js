eca254b726c9c730efc25e8e0bc934f60bac8a7f
JGAlvarez <joao.g.m.alvarez@gmail.com>
2025-04-16 15:54:33 -0300

    Mask for filtro de produtos

--- static/js/filtro_coluna_macro_produtos.js
+++ static/js/filtro_coluna_macro_produtos.js
@@ -1,3 +1,4 @@
+// Popup de indicação de uso e variação das faces
 const indicUsoButton = document.querySelector(".indicUsoButton");
 const indicUsowindow = document.querySelector(".indicUsoWindow");
 const indicUsoJanelaClose = document.querySelector(".indicUsoJanelaClose");
@@ -48,4 +49,19 @@ variacaoFacesJanelaClose.addEventListener("click", () => {
     else {
         variacaoFacesWindow.style.display = "none";
     }
+});
+
+$(document).ready(function() {
+    $('.precoInput').inputmask("currency", {
+        prefix: "R$ ",
+        radixPoint: ",",
+        digits: 2,
+        digitsOptional: false,
+        placeholder: "0",
+        rightAlign: false,
+        removeMaskOnSubmit: true,  // Optional: useful when posting the form
+        autoUnmask: true,           // Optional: removes formatting on input's .val()
+        showMaskOnHover: false,     // Optional: remove mask on hover
+        showMaskOnFocus: false,      // Optional: show mask on focus only
+    });
 });
